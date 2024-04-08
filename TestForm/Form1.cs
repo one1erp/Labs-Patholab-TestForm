6d722290339f2aee0d4980867b507bf5e3320970
one1erp <avigaile@one1.co.il>
2024-04-08 17:33:56 +0300

    Initial commit with additional files

---
+++ TestForm/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+
+            //var e = new Entities();
+            //var q=e.ADDRESS.ToList();
+            //var b = q.First();
+            //MessageBox.Show(b.ADDRESS_TABLE_NAME);
+
+            InitializeComponent();
+            //System.Data.Common.DbProviderFactories.GetFactoryClasses();
+            LaborantRegitrationWpf.WpfShipmentCtl wpfReports = new LaborantRegitrationWpf.WpfShipmentCtl(null,null, null, null, null
+                                                                                              );
+            wpfReports.DEBUG = true;
+            wpfReports.InitializeData();
+            this.elementHost1.Child = wpfReports;
+        }
+    }
+}
